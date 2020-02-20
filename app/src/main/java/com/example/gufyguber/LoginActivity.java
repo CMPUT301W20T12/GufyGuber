20b26fc6a8cb0d0d99b085e2adcf09c2bbbea04a
Dalton Ronan <dronan@ualberta.ca>
2020-02-20 12:46:02 -0700

    added mip map files for GufyGuber Logo in project; saved logo png in 'doc' folder; made layout and java file for login activity

--- app/src/main/java/com/example/gufyguber/LoginActivity.java
+++ app/src/main/java/com/example/gufyguber/LoginActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
