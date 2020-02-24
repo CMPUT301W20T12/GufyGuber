6ccb119bf5cc851ff11765d692a6e07ac74032c7
Dalton Ronan <dronan@ualberta.ca>
2020-02-24 11:45:00 -0700

    added firebase authorization to create user for email and password

--- app/src/main/java/com/example/gufyguber/RegisterUserActivity.java
+++ app/src/main/java/com/example/gufyguber/RegisterUserActivity.java
@@ -3,16 +3,27 @@ package com.example.gufyguber;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class RegisterUserActivity extends AppCompatActivity {
+    private static final String TAG = "EmailPassword";
     EditText username;
     EditText email;
     EditText firstName;
@@ -26,11 +37,14 @@ public class RegisterUserActivity extends AppCompatActivity {
     EditText seatNumber;
     Button register;
 
+    private FirebaseAuth mAuth;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         Intent intent = getIntent();
         String userType = intent.getStringExtra("userType");
@@ -46,7 +60,6 @@ public class RegisterUserActivity extends AppCompatActivity {
 
         }
 
-
         username = findViewById(R.id.user_name);
         email = findViewById(R.id.email);
         firstName = findViewById(R.id.first_name);
@@ -56,13 +69,78 @@ public class RegisterUserActivity extends AppCompatActivity {
         confirmPassword = findViewById(R.id.confirm_password);
         register = findViewById(R.id.register);
 
-
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
+                createAccount(email.getText().toString(), password.getText().toString());
+                
             }
         });
-
     }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterUserActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        ArrayList<String> fieldArray = new ArrayList<String>;
+
+        String usernameStr = username.getText().toString();
+        fieldArray.add(usernameStr);
+        String emailStr = email.getText().toString();
+        fieldArray.add(emailStr);
+        String firstNameStr = firstName.getText().toString();
+        fieldArray.add(firstNameStr);
+        String lastNameStr = lastName.getText().toString();
+        fieldArray.add(lastNameStr);
+        String phoneStr = phone.getText().toString();
+        fieldArray.add(phoneStr);
+        String passwordStr = password.getText().toString();
+        fieldArray.add(passwordStr);
+        String confirmPasswordStr = confirmPassword.getText().toString();
+        fieldArray.add(confirmPasswordStr);
+
+//        for (String field : fieldArray) {
+//            if (TextUtils.isEmpty(field)) {
+//
+//            }
+//        }
+
+        if (TextUtils.isEmpty(emailStr)) {
+            email.setError("Required.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        if (TextUtils.isEmpty(passwordStr)) {
+            password.setError("Required.");
+            valid = false;
+        } else {
+            password.setError(null);
+        }
+
+        return valid;
+    }
+
 }
