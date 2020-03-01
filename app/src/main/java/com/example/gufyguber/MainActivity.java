905519e4839870dd9e34635a7c2037c75727a422
Kenz Bauer <mabauer@ualberta.ca>
2020-03-01 16:00:32 -0700

    ability to scan

--- app/src/main/java/com/example/gufyguber/MainActivity.java
+++ app/src/main/java/com/example/gufyguber/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.gufyguber;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import javax.annotation.Nullable;
+
 public class MainActivity extends AppCompatActivity {
+    Button scan;
+    TextView result;
+
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +30,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        scan = findViewById(R.id.scan);
+        result = findViewById(R.id.result);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, Scan.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if(data != null) {
+                final Barcode barcode = data.getParcelableExtra("barcode");
+                result.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(barcode.displayValue);
+                    }
+                });
+            }
+        }
     }
 }
