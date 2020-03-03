483b7e0a93dacaf14314c08e8023d06e5e79646f
Kenz Bauer <mabauer@ualberta.ca>
2020-03-03 12:37:18 -0700

    qr code generated

--- app/src/main/java/com/example/gufyguber/MainActivity.java
+++ app/src/main/java/com/example/gufyguber/MainActivity.java
@@ -2,11 +2,25 @@ package com.example.gufyguber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
+    Button generate;
+    BitMatrix matrix;
+    Bitmap map;
+    String codeMessage;
+    ImageView qrCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        generate = findViewById(R.id.generate);
+        qrCode = findViewById(R.id.qrCode);
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeMessage = "Trial!!!";
+                MultiFormatWriter multi = new MultiFormatWriter();
+                try {
+                    matrix = multi.encode(codeMessage, BarcodeFormat.QR_CODE, 200, 200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    map = barcodeEncoder.createBitmap(matrix);
+                    qrCode.setImageBitmap(map);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
