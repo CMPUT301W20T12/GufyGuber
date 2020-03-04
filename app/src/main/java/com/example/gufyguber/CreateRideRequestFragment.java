f209cb5640ac4e6c6f2d164494e37f9997845807
Robert MacGillivray <rmacgill@ualberta.ca>
2020-03-03 17:09:31 -0700

    Update CreateRideRequestFragment.java
    - Basic fragment functionality for creating ride requests
    - Not integrated with the map stuff yet

--- app/src/main/java/com/example/gufyguber/CreateRideRequestFragment.java
+++ app/src/main/java/com/example/gufyguber/CreateRideRequestFragment.java
@@ -23,14 +23,82 @@
 
 package com.example.gufyguber;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class CreateRideRequestFragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.type.LatLng;
+
+public class CreateRideRequestFragment extends DialogFragment {
+
+    public interface CreateRideRequestListener {
+        void onRideRequestCreated (RideRequest newRideRequest);
+    }
 
     //TODO: I'm thinking about this and realizing that by the time the user gets here, they really
     //      only need to enter their fair fare since we pull the rest from behind the scenes, and
     //      should have selected the start and end points first. Perhaps we should go to the map from
     //      this screen instead of going to this screen from the map?
 
+    private CreateRideRequestListener onCreatedListener;
+
     private EditText fareEditText;
+
+    @Override
+    public void onAttach(Context context)  {
+        super.onAttach(context);
+        if (context instanceof CreateRideRequestListener) {
+            onCreatedListener = (CreateRideRequestListener)context;
+        } else {
+            onCreatedListener = null;
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle ssavedInstanceState) {
+        final View view = LayoutInflater.from(getActivity()).inflate(R.layout.create_ride_request_layout, null);
+        fareEditText = view.findViewById(R.id.fare_EditText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext())
+                .setView(view)
+                .setTitle("Create Ride Request")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Send Request", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (validateEntries()) {
+                            LatLng testPickup = LatLng.newBuilder().setLatitude(13).setLongitude(13).build();
+                            LatLng testDropoff = LatLng.newBuilder().setLatitude(31).setLongitude(31).build();
+                            RideRequest newRequest = new RideRequest("TestRiderUID", Float.parseFloat(fareEditText.getText().toString()), testPickup, testDropoff);
+                            if (onCreatedListener != null) {
+                                onCreatedListener.onRideRequestCreated(newRequest);
+                            }
+                        } else {
+                            Toast.makeText(getContext(), "Missing Required Info", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        return builder.create();
+    }
+
+    /**
+     * A helper to check that all required fields have entries
+     * @return True if all required information has been entered in the fragment, false otherwise
+     */
+    private boolean validateEntries() {
+        //TODO: Probably have more to validate
+        return (!TextUtils.isEmpty(fareEditText.getText().toString()));
+    }
 }
