6b85bebdb7a01fa952d0f4963de2eac32972791f
Harrison Peters <hmp@ualberta.ca>
2020-03-19 15:24:06 -0600

    Driver can now view rider contact info

--- app/src/main/java/com/example/gufyguber/ui/CurrentRequest/CurrentRequestFragment.java
+++ app/src/main/java/com/example/gufyguber/ui/CurrentRequest/CurrentRequestFragment.java
@@ -36,6 +36,7 @@ import com.example.gufyguber.OfflineCache;
 import com.example.gufyguber.R;
 import com.example.gufyguber.RideRequest;
 import com.example.gufyguber.Rider;
+import com.example.gufyguber.User;
 import com.example.gufyguber.ui.Profile.UserContactInformationFragment;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -144,6 +145,7 @@ public class CurrentRequestFragment extends Fragment {
                         riderText.setText("Rider Unavailable");
                     } else {
                         riderText.setText(String.format("%s %s", value.getFirstName(), value.getLastName()));
+                        makeNameClickable(value, riderText);
                     }
                 }
             });
@@ -206,13 +208,13 @@ public class CurrentRequestFragment extends Fragment {
 
     }
 
-    private void makeNameClickable(final Driver driver, TextView driverText){
+    private void makeNameClickable(final User user, TextView driverText){
         driverText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Bundle bundle = new Bundle();
-                bundle.putString("email", driver.getEmail());
-                bundle.putString("phone", driver.getPhoneNumber());
+                bundle.putString("email", user.getEmail());
+                bundle.putString("phone", user.getPhoneNumber());
                 UserContactInformationFragment infoFragment = new UserContactInformationFragment();
                 infoFragment.setArguments(bundle);
                 infoFragment.show(getFragmentManager(), "user_contact_information");
