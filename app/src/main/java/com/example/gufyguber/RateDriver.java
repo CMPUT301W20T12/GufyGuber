2d10e51172821276b38c7c22104b664977d2abb7
Kenz Bauer <mabauer@ualberta.ca>
2020-03-26 00:53:48 -0600

    added javadocs to FirebaseManager Functions

--- app/src/main/java/com/example/gufyguber/RateDriver.java
+++ app/src/main/java/com/example/gufyguber/RateDriver.java
@@ -26,7 +26,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -35,10 +34,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.SetOptions;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class RateDriver extends AppCompatActivity {
     ImageButton thumbsUp;
