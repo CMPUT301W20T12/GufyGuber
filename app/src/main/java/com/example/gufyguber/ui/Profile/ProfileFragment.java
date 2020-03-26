913ef2f9e2d140ba5e44dbb9fb83aca14d4c4455
Harrison Peters <hmp@ualberta.ca>
2020-03-25 20:01:50 -0600

    Overhauled Profile fragment validateForm()

--- app/src/main/java/com/example/gufyguber/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/gufyguber/ui/Profile/ProfileFragment.java
@@ -25,6 +25,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -52,6 +53,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.telephony.PhoneNumberUtils.formatNumber;
 
@@ -166,7 +169,6 @@ public class ProfileFragment extends Fragment {
                 // when user clicks the edit button, change the fields to be editable
                 firstNameText.setEnabled(true);
                 lastNameText.setEnabled(true);
-                emailText.setEnabled(true);
                 phoneText.setEnabled(true);
                 phoneText.addTextChangedListener(new PhoneNumberFormattingTextWatcher("CA"));
                 editProfile.setVisibility(View.GONE);
@@ -285,23 +287,60 @@ public class ProfileFragment extends Fragment {
      *  Return true if all fields are filled, false if any are empty
      */
     private boolean validateForm() {
-        String phoneRegex = "^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$"; //https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
-        if (driver) {
-            return (!TextUtils.isEmpty(emailText.getText().toString()) &&
-                    !TextUtils.isEmpty(firstNameText.getText().toString()) &&
-                    !TextUtils.isEmpty(lastNameText.getText().toString()) &&
-                    !TextUtils.isEmpty(phoneText.getText().toString()) &&
-                    phoneText.getText().toString().matches(phoneRegex) &&
-                    !TextUtils.isEmpty(makeText.getText().toString()) &&
-                    !TextUtils.isEmpty(modelText.getText().toString()) &&
-                    !TextUtils.isEmpty(plateText.getText().toString()) &&
-                    !TextUtils.isEmpty(seatText.getText().toString()));
-        } else {
-            return (!TextUtils.isEmpty(emailText.getText().toString()) &&
-                    !TextUtils.isEmpty(firstNameText.getText().toString()) &&
-                    !TextUtils.isEmpty(lastNameText.getText().toString()) &&
-                    !TextUtils.isEmpty(phoneText.getText().toString()) &&
-                    phoneText.getText().toString().matches(phoneRegex));
+        String phoneRegex = "^(\\+?\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$"; //https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
+//        if (driver) {
+//            return (!TextUtils.isEmpty(emailText.getText().toString()) &&
+//                    !TextUtils.isEmpty(firstNameText.getText().toString()) &&
+//                    !TextUtils.isEmpty(lastNameText.getText().toString()) &&
+//                    !TextUtils.isEmpty(phoneText.getText().toString()) &&
+//                    phoneText.getText().toString().matches(phoneRegex) &&
+//                    !TextUtils.isEmpty(makeText.getText().toString()) &&
+//                    !TextUtils.isEmpty(modelText.getText().toString()) &&
+//                    !TextUtils.isEmpty(plateText.getText().toString()) &&
+//                    !TextUtils.isEmpty(seatText.getText().toString()));
+//        } else {
+//            return (!TextUtils.isEmpty(emailText.getText().toString()) &&
+//                    !TextUtils.isEmpty(firstNameText.getText().toString()) &&
+//                    !TextUtils.isEmpty(lastNameText.getText().toString()) &&
+//                    !TextUtils.isEmpty(phoneText.getText().toString()) &&
+//                    phoneText.getText().toString().matches(phoneRegex));
+//        }
+
+        Boolean isValid = true;
+
+        HashMap<EditText, String> fields = new HashMap<>();
+
+        fields.put((EditText) firstNameText, firstNameText.getText().toString());
+        fields.put((EditText) lastNameText, lastNameText.getText().toString());
+        fields.put((EditText) phoneText, phoneText.getText().toString());
+        //fields.put((EditText) phoneText, phoneText.getText().toString());
+        if(driver){
+            fields.put((EditText) makeText, makeText.getText().toString());
+            fields.put((EditText) modelText, modelText.getText().toString());
+            fields.put((EditText) plateText, plateText.getText().toString());
+            fields.put((EditText) seatText, seatText.getText().toString());
         }
+
+        for (Map.Entry field : fields.entrySet()) {
+            /* iterate over Map and check all fields are filled in*/
+            if (TextUtils.isEmpty((String) field.getValue())) {
+                ((EditText) field.getKey()).setError("Required");   /* if not filled in, flag */
+                isValid = false;
+            } else {
+                ((EditText) field.getKey()).setError(null);                 /* else, update counter */
+            }
+        }
+
+        if(isValid){
+            if(!phoneText.getText().toString().matches(phoneRegex)) {
+                phoneText.setError("Invalid Phone Number");
+                isValid = false;
+            }
+        }
+
+        return isValid;
     }
+
+
+
 }
